137d3832ccbd84b80c6a5f58d353f5ea70350372
Deuti Gurung <gurungdeuti@gmail.com>
2022-09-19 18:01:24 +0545

    home component design update and banner component add

---
+++ resources/js/components/Banner.js
@@ -0,0 +1,57 @@
+import React from "react";
+import banner1 from "../img/banner-1.jpg";
+import banner2 from "../img/banner-2.jpg";
+import banner3 from "../img/banner-3.jpg";
+
+function Banner(){
+    return(
+        <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+            <div className="carousel-item active">
+                <img src={banner3} className="bd-placeholder-img" width="100%" height="100%" />
+                <div className="container">
+                <div className="carousel-caption text-start">
+                    <h1>Example headline.</h1>
+                    <p>Some representative placeholder content for the first slide of the carousel.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                </div>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <img src={banner2} className="bd-placeholder-img" width="100%" height="100%" />
+                <div className="container">
+                <div className="carousel-caption">
+                    <h1>Another example headline.</h1>
+                    <p>Some representative placeholder content for the second slide of the carousel.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                </div>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <img src={banner1} className="bd-placeholder-img" width="100%" height="100%" />
+                <div className="container">
+                <div className="carousel-caption text-end">
+                    <h1>One more for good measure.</h1>
+                    <p>Some representative placeholder content for the third slide of this carousel.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                </div>
+                </div>
+            </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    );
+}
+export default Banner;
