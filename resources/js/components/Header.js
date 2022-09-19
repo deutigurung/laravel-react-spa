25b44a111c956077d222d91fb97bdc137174509b
Deuti Gurung <gurungdeuti@gmail.com>
2022-09-19 16:33:50 +0545

    display component on navlink click

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,6 +1,7 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React ,{ useEffect } from "react";
+import {Link ,useLocation} from "react-router-dom";
  function Header () {
+    const location = useLocation();
     const pageUrl = [
         {
             "name":"Home","url":"/",
@@ -15,25 +16,35 @@ import {Link} from "react-router-dom";
             "name":"Contact","url":"/contact",
         }
     ];
+    //useEffect is similar to componentDidMount and componentDidUpate
+    useEffect(() => {
+        pageUrl.map((page)=>{
+            if(page.url == location.pathname) {
+                document.title = page.name;
+            }
+        });
+    }, [])
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-             <Link to="/" className="navbar-brand">Laravel</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div className="container">
+            <Link to="/" className="navbar-brand">Laravel</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
  
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
                     {
                         pageUrl.map((page, key) => {
                             return (
-                                <li key={key}>
-                                    <Link to={page.url} className="nav-link">{page.name}</Link>
+                                <li key={key} className="nav-item">
+                                    <Link to={page.url} className={`nav-link ${location.pathname == page.url ? 'active' : ''}`}>{page.name}</Link>
                                 </li>
                             )
                         })
                     }
                 </ul>
+            </div>
             </div>
         </nav>
     );
